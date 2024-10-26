3ce8bc0b9fa01ac6de1d9efd566d1b2a6c5e6dbd
Kris <akriscastillo@gmail.com>
2024-10-26 01:02:20 -0500

    Fixing more interface views and making a quick fix to the routes navgetarion in the ProfileSelectionPage

--- Views/ProfileSelectionPage.xaml.cs
+++ Views/ProfileSelectionPage.xaml.cs
@@ -6,4 +6,8 @@ public partial class ProfileSelectionPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void ToProfile(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProfilePage());
+    }
 }
--- obj/Debug/net8.0-maccatalyst/maccatalyst-x64/HealthCare.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-x64/HealthCare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d5e746c3e6e6c842482df9251e7c846b88a036ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5c9e2be5c1c8c85324031d173e39d3d29e40f074")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
