4e9022669e6a53143f972d05e25bc15ebabf7187
manuelalrod <manuelalmengor32@gmail.com>
2024-11-11 00:48:09 -0500

    save

--- obj/Debug/net8.0-android/HealthCare.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/HealthCare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cd2b79add48ca5b1a803a79a3f8b054861a78b96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ba4bfdd5ee2e8c3392bf56ab246941f6c20ac8ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
