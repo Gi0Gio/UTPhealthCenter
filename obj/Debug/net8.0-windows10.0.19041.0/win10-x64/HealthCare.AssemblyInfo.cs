c688bd9389cb25792d008c81885ae42052ee03ee
manuelalrod <manuelalmengor32@gmail.com>
2024-12-04 00:44:53 -0500

    getting design user

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/HealthCare.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/HealthCare.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+aea08520395a0237b052548bc9173a639528bd60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bf7b4a2d321d9fd2761483edab74a6975bef810c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
