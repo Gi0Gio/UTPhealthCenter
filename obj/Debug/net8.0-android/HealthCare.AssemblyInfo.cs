19740f81bbbced4ff28831ed8d49059832eb2d36
manuelalrod <manuelalmengor32@gmail.com>
2024-12-04 16:41:11 -0500

    getting profile

--- obj/Debug/net8.0-android/HealthCare.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/HealthCare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+840d24bb73f74e5df753a53e4bd3a7f61b395cac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f863edf56038e7fb57d087ed72b15e61edd132a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
